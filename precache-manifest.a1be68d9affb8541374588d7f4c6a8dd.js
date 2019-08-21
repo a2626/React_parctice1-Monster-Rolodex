4aab194291fa4353aa30e8874024347b9c7f55e9
a2626 <lucas091750@gmail.com>
2019-08-21 21:26:59 +1000

    Updates

---
+++ precache-manifest.a1be68d9affb8541374588d7f4c6a8dd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f575c8494e61e1193bd35464a842314",
+    "url": "https;//a2626.github.io/React_parctice1-Monster-Rolodex/index.html"
+  },
+  {
+    "revision": "98427410aab5786614fe",
+    "url": "https;//a2626.github.io/React_parctice1-Monster-Rolodex/static/css/main.6290f81b.chunk.css"
+  },
+  {
+    "revision": "db7ebd1ab4ecf98a6e23",
+    "url": "https;//a2626.github.io/React_parctice1-Monster-Rolodex/static/js/2.9dbee6a0.chunk.js"
+  },
+  {
+    "revision": "98427410aab5786614fe",
+    "url": "https;//a2626.github.io/React_parctice1-Monster-Rolodex/static/js/main.af2e4c85.chunk.js"
+  },
+  {
+    "revision": "e782ae451b26bf7404d9",
+    "url": "https;//a2626.github.io/React_parctice1-Monster-Rolodex/static/js/runtime~main.1ff14904.js"
+  }
+]);
